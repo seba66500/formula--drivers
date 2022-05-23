4f4ace224649d98d2e27f08888f2e47771fe11a9
seba66500 <sebasti66500@wp.pl>
2022-05-23 18:17:25 +0200

    Creating layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Projekt F1</title>
-    <link rel="stylesheet" href="{{asset('css/main.css')}}">
-</head>
-<body>
-    <header>
-        <ul>
-            <li><a href="">Main page</a></li>
-            <li><a href="">List of drivers</a></li>
-            <li><a href="">Drivers championship table</a></li>
-            <li><a href="">Penalty points</a></li>
-            <li><a href="">Reprimands</a></li>
-        </ul>
-    </header>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    This is my first laravel page 
+@stop
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Formula drivers</title>
+    <link rel="stylesheet" href="{{asset('css/main.css')}}">
+</head>
+<body>
+    <header>
+        <ul>
+            <li><a href="">Main page</a></li>
+            <li><a href="">List of drivers</a></li>
+            <li><a href="">Drivers championship table</a></li>
+            <li><a href="">Penalty points</a></li>
+            <li><a href="">Reprimands</a></li>
+        </ul>
+    </header>
+    <section>
+        @yield('content')
+    </section>
+</body>
+</html>
